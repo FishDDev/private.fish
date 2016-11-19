ae9d9b5b3f0375a6b37353438407cb1a87fd5fcf
Fish <i@emailemail.cn>
2016-11-19 08:52:04 +0800

    Create shadowsocks-libev.sh

---
+++ install/shadowsocks-libev.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/shadowsocks-libev.sh
+chmod +x shadowsocks-libev.sh
+./shadowsocks-libev.sh 2>&1 | tee shadowsocks-libev.log
