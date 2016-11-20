0a90108e7054ff11b499457bb608bc18fb366ea7
Fish <i@emailemail.cn>
2016-11-20 21:06:05 +0800

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+rm -rf serverspeeder.sh shadowsocks-libev.sh yum.install.sh upconfig.sh >/dev/null
+
 echo "Asia/Shanghai" >/etc/timezone
 /etc/init.d/shadowsocks restart
 /serverspeeder/bin/serverSpeeder.sh restart
