e7a91eb8c03796f50951237dddf3a66d86936652
Fish <i@emailemail.cn>
2016-11-21 07:16:05 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ chkinstall_ss()
     if [ -f /usr/local/bin/ss-server ] ; then
         echo "Shadowsocks-libev 已安装"    
     else
-        wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/install/shadowsocks-libev.sh &&
+        wget --no-check-certificate -O -b shadowsocks-libev.sh https://raw.githubusercontent.com/FishDDev/tools/Privated/install/shadowsocks-libev.sh &&
           chmod +x shadowsocks-libev.sh;
           echo "Shadowsocks-libev 安装引导文件已下载完成" || 
           echo "Shadowsocks-libev 安装引导文件下载出错"
