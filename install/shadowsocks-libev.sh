a9b3f61e3037ed93ac07312d14944af9f25f6743
Fish <i@emailemail.cn>
2016-11-20 16:21:08 +0800

    Delete shadowsocks-libev.sh

--- install/shadowsocks-libev.sh
+++
@@ -1,3 +0,0 @@
-wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/shadowsocks-libev.sh
-chmod +x shadowsocks-libev.sh
-./shadowsocks-libev.sh 2>&1 | tee shadowsocks-libev.log
