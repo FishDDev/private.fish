259c144fbcafd51db57bae6142e77f7e609093d8
Fish <i@emailemail.cn>
2016-11-21 09:51:06 +0800

    Update yum.install.sh

--- yum.install.sh
+++ yum.install.sh
@@ -2,31 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-
-#wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-#rpm -ivh epel-release-X-
-
-chmod +x ./yum.install.sh
-
-#wget --no-check-certificate https://github.com/FishDDev/tools/archive/Privated.zip
-
-#get shadowsocks-libev
-rm -rf shadowsocks-libev.sh
-wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/install/shadowsocks-libev.sh
-chmod +x shadowsocks-libev.sh
-
-#get serverspeeder
-rm -rf serverspeeder.sh
-wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/install/serverspeeder.sh
-chmod +x serverspeeder.sh
-
-rm -rf upconfig.sh
-wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/upconfig.sh
-chmod +x upconfig.sh
-
-rm -rf reload.sh
+rm -rf ./reload.sh
 wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/reload.sh
-chmod +x reload.sh
+chmod +x ./reload.sh
+cp -f ./reload.sh /usr/bin/reload
+chmod +x /usr/bin/reload
 
 
 #yum
