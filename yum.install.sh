3fc1ce499ea764d51495c5bf822540394762a975
Fish <FishDDev@users.noreply.github.com>
2016-11-23 18:13:18 +0800

    Update yum.install.sh

--- yum.install.sh
+++ yum.install.sh
@@ -2,11 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-#By:      Fish
-#mailto:  fishdev@qq.com
+##by:      Fish
+##mailto:  fishdev@qq.com
 
 
-#update reload script
+# update reload script
 rm -rf ./reload.sh
 rm -rf /usr/bin/reload
 wget --no-check-certificate https://raw.githubusercontent.com/FishDDev/tools/Privated/reload.sh
@@ -14,18 +14,19 @@ cp -f ./reload.sh /usr/bin/reload
 chmod +x ./reload.sh
 chmod +x /usr/bin/reload
 
-#yum clean & update & upgrade
+# yum clean & update & upgrade
 yum clean all
 yum update 
 yum upgrade
 
-#yum & pip install packages
+# yum & pip install packages
 yum install -y libnet libpcap libnet-devel libpcap-devel net-tools python-pip libevent
 pip install --upgrade -I pip
 pip install -I greenlet gevent
 
-#remove old script
+# remove old script
 rm -rf ./yum.install.sh
 rm -rf ./reload
 
+# run reload
 reload
