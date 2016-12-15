d56c22a16a9833a0dd4bae96ab44a8aa7cd58b7e
Fish <FishDDev@users.noreply.github.com>
2016-12-15 12:07:03 +0800

    Update

--- reload.sh
+++ reload.sh
@@ -77,63 +77,10 @@ chkinstall_serverspeeder()
 optimized_shadowsocks()
 {
     if ! grep -q "* soft nofile" /etc/security/limits.conf; then
-        echo -e "* soft nofile 512000\n* hard nofile 1024000" >> /etc/security/limits.conf
+        echo -e "* soft nofile 51200\n* hard nofile 51200" >> /etc/security/limits.conf
     fi
-    
-    if ! grep -q "ulimit -SHn 1024000" /etc/profile; then
-        echo -e "ulimit -SHn 1024000" >> /etc/profile
-    fi
-    
-    ulimit -SHn 1024000
-    
-    if ! [ -f /etc/sysctl.d/local.conf ] ; then
-    cat > /etc/sysctl.d/local.conf<-EOF
-#Google BBR
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
-# max open files
-fs.file-max = 1024000
-# max read buffer
-net.core.rmem_max = 67108864
-# max write buffer
-net.core.wmem_max = 67108864
-# default read buffer
-net.core.rmem_default = 65536
-# default write buffer
-net.core.wmem_default = 65536
-# max processor input queue
-net.core.netdev_max_backlog = 4096
-# max backlog
-net.core.somaxconn = 4096
-# resist SYN flood attacks
-net.ipv4.tcp_syncookies = 1
-# reuse timewait sockets when safe
-net.ipv4.tcp_tw_reuse = 1
-# turn off fast timewait sockets recycling
-net.ipv4.tcp_tw_recycle = 0
-# short FIN timeout
-net.ipv4.tcp_fin_timeout = 30
-# short keepalive time
-net.ipv4.tcp_keepalive_time = 1200
-# outbound port range
-net.ipv4.ip_local_port_range = 10000 65000
-# max SYN backlog
-net.ipv4.tcp_max_syn_backlog = 4096
-# max timewait sockets held by system simultaneously
-net.ipv4.tcp_max_tw_buckets = 5000
-# TCP receive buffer
-net.ipv4.tcp_rmem = 4096 87380 67108864
-# TCP write buffer
-net.ipv4.tcp_wmem = 4096 65536 67108864
-# turn on path MTU discovery
-net.ipv4.tcp_mtu_probing = 1
-# forward ivp4
-net.ipv4.ip_forward = 1
-EOF
-    sysctl -p >>$relog 2>&1
-    sysctl --system >>$relog 2>&1
-    sysctl -p /etc/sysctl.d/local.conf >>$relog 2>&1
-    fi
+
+    ulimit -SHn 51200
 }
 
 
