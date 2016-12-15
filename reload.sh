bd6987d99e29ccd60c968f21c43d54ba141cb850
Fish <FishDDev@users.noreply.github.com>
2016-12-15 08:37:39 +0800

    Update

--- reload.sh
+++ reload.sh
@@ -83,31 +83,12 @@ optimized_shadowsocks()
     
     if ! [ -f /etc/sysctl.d/local.conf ] ; then
     cat > /etc/sysctl.d/local.conf<<-EOF
-fs.file-max = 65535
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-net.core.rmem_default = 65536
-net.core.wmem_default = 65536
-net.core.netdev_max_backlog = 4096
-net.core.somaxconn = 4096
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_tw_recycle = 0
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_keepalive_time = 1200
-net.ipv4.ip_local_port_range = 10000 65000
-net.ipv4.tcp_max_syn_backlog = 4096
-net.ipv4.tcp_max_tw_buckets = 5000
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
-net.ipv4.tcp_mtu_probing = 1
-
 #Google BBR
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 EOF
     fi
+    sysctl -p >>$relog 2>&1
     sysctl --system >>$relog 2>&1
     sysctl -p /etc/sysctl.d/local.conf >>$relog 2>&1
 }
